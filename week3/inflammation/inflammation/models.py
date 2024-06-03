f475de0ea0132305996645ae13607f1295443f74
samvidav <samvida.venkatesh@gmail.com>
2024-06-03 12:46:14 +0100

    Add checks for 2D arrays

--- week3/inflammation/inflammation/models.py
+++ week3/inflammation/inflammation/models.py
@@ -35,11 +35,19 @@ def daily_min(data):
 def patient_normalise(data):
     """
     Normalise patient data from a 2D inflammation data array.
+    
+    Errors are raised for values that are not in a 2D ndarray. 
 
     NaN values are ignored, and normalised to 0.
 
-    Negative values are rounded to 0.
+    Negative values are rounded to 0 after issuing an error.
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('Inflammation data should be an ndarray object')
+    if len(data.shape) != 2:
+        raise ValueError('Inflammation data array is not 2D')
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
     max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]
--- week3/inflammation/tests/test_models.py
+++ week3/inflammation/tests/test_models.py
@@ -52,17 +52,26 @@ def test_daily_min_string():
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 @pytest.mark.parametrize(
-    "test, expected",
+    "test, expected, expect_raises",
     [
-        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
-        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
-        ([[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]], [[0, 1, 1], [1, 1, 1], [1, 1, 1]]),
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
-        ([[-1, 2, 3], [4, -5, 6], [7, 8, -9]], [[0, 0.67, 1], [0.67, 0, 1], [0.88, 1, 0]]),
+        ('This is not an array', None, TypeError),
+        (6, None, TypeError),
+        (np.zeros((2, 3, 4)), None, ValueError),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        ([[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]], [[0, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], None),
+        ([[-1, 2, 3], [4, -5, 6], [7, 8, -9]], [[0, 0.67, 1], [0.67, 0, 1], [0.88, 1, 0]], ValueError),
     ])
 
-def test_patient_normalise(test, expected):
+def test_patient_normalise(test, expected, expect_raises):
     """Test normalisation works for arrays of one and positive integers.
        Assumption that test accuracy of two decimal places is sufficient."""
     from inflammation.models import patient_normalise
-    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
